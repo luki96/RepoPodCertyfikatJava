26e935e7b5f6b3956ea895a37d1ed640b4b30775
luki96 <fornal96@o2.pl>
2015-07-15 00:10:04 +0200

    1 commit na develu

--- src/test/Testowa.java
+++ src/test/Testowa.java
@@ -2,5 +2,14 @@ package test;
 
 public class Testowa
 {
-
+	Runnable abc = new Runnable()
+	{
+		
+		@Override
+		public void run()
+		{
+			// TODO Auto-generated method stub
+			
+		}
+	};
 }
---
+++ src/test/TestowaRunnable.java
@@ -0,0 +1,11 @@
+package test;
+
+public class TestowaRunnable implements Runnable
+{
+	@Override
+	public void run()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+}
